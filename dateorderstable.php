879bdc8884df2bea85da9a4e89dc2ed0b373a9f8
DtheRock <29987671+DtheRock@users.noreply.github.com>
2022-12-24 12:39:50 +0200

    Update dateorderstable.php

--- dateorderstable.php
+++ dateorderstable.php
@@ -7,19 +7,19 @@ if ( ! class_exists( 'WP_List_Table' ) ) {
 class CP_Orders_With_Delivery_Dates_Table extends WP_List_Table {
     public function __construct() {
         parent::__construct( array(
-            'singular' => __('Order with Delivery Date', 'customize-product-delivery-date'),
-            'plural'   => __('Orders with Delivery Dates', 'customize-product-delivery-date'),
+            'singular' => esc_html__('Order with Delivery Date', 'customize-product-delivery-date'),
+            'plural'   => esc_html__('Orders with Delivery Dates', 'customize-product-delivery-date'),
             'ajax'     => false
         ));
     }
 
     public function get_columns() {
         $columns = array(
-            'order_id'        => __('Order ID', 'customize-product-delivery-date'),
-            'customer_name'   => __('Customer Name', 'customize-product-delivery-date'),
-            'order_date'      => __('Order Date', 'customize-product-delivery-date'),
-            'delivery_date'   => __('Delivery Date', 'customize-product-delivery-date'),
-            'two_days'        => __('Within 2 days?', 'customize-product-delivery-date')
+            'order_id'        => esc_html__('Order ID', 'customize-product-delivery-date'),
+            'customer_name'   => esc_html__('Customer Name', 'customize-product-delivery-date'),
+            'order_date'      => esc_html__('Order Date', 'customize-product-delivery-date'),
+            'delivery_date'   => esc_html__('Delivery Date', 'customize-product-delivery-date'),
+            'two_days'        => esc_html__('Within 2 days?', 'customize-product-delivery-date')
         );
         return $columns;
     }
@@ -30,6 +30,7 @@ class CP_Orders_With_Delivery_Dates_Table extends WP_List_Table {
         $sortable = array();
         $this->_column_headers = array( $columns, $hidden, $sortable );
         $this->process_bulk_action();
+
         $per_page = 10;
         $current_page = $this->get_pagenum();
         $total_items = self::record_count();
@@ -37,45 +38,46 @@ class CP_Orders_With_Delivery_Dates_Table extends WP_List_Table {
             'total_items' => $total_items,
             'per_page'    => $per_page
         ));
+
         $this->items = self::get_orders_with_delivery_dates( $per_page, $current_page );
     }
 
-   public function column_default( $item, $column_name ) {
-    switch ( $column_name ) {
-        case 'order_id':
-        case 'customer_name':
-        case 'order_date':
-        case 'delivery_date':
-        case 'two_days':
-            return $item[ $column_name ];
-        default:
-            return print_r( $item, true );
+    public function column_default( $item, $column_name ) {
+        switch ( $column_name ) {
+            case 'order_id':
+            case 'customer_name':
+            case 'order_date':
+            case 'delivery_date':
+            case 'two_days':
+                return esc_html( $item[ $column_name ] );
+            default:
+                return print_r( $item, true );
+        }
     }
-}
 
     public function column_two_days( $item ) {
-        $delivery_date = new DateTime( $item['delivery_date'] );
+        $delivery_date = new DateTime( sanitize_text_field( $item['delivery_date'] ) );
         $now = new DateTime();
         $diff = date_diff( $now, $delivery_date );
         if ( $diff->days < 2 ) {
-            return '<p style="background-color:#FFA07A; text-align: center; max-width:50%;">'.__('Yes', 'customize-product-delivery-date').'</p>';
+            return '<p style="background-color:#FFA07A; text-align: center; max-width:50%;">'.esc_html__('Yes', 'customize-product-delivery-date').'</p>';
         } else {
-            return '<p style="background-color:green; text-align: center; max-width:50%;">'.__('No', 'customize-product-delivery-date').'</p>';
+            return '<p style="background-color:green; text-align: center; max-width:50%;">'.esc_html__('No', 'customize-product-delivery-date').'</p>';
         }
     }
 
     public function column_delivery_date( $item ) {
-        $delivery_date = new DateTime( $item['delivery_date'] );
-        return $delivery_date->format('Y-m-d');
+        $delivery_date = new DateTime( sanitize_text_field( $item['delivery_date'] ) );
+		return esc_html( $delivery_date->format('Y-m-d') );
     }
 
     public function column_order_date( $item ) {
-        $order_date = new DateTime( $item['order_date'] );
-        return $order_date->format('Y-m-d');
+        $order_date = new DateTime( sanitize_text_field( $item['order_date'] ) );
+		return esc_html( $order_date->format('Y-m-d') );
     }
 
     public function row_class( $item ) {
-        $delivery_date = new DateTime( $item['delivery_date'] );
+        $delivery_date = new DateTime( sanitize_text_field( $item['delivery_date'] ) );
         $now = new DateTime();
         $diff = date_diff( $now, $delivery_date );
         if ( $diff->days < 2 ) {
@@ -91,12 +93,20 @@ class CP_Orders_With_Delivery_Dates_Table extends WP_List_Table {
                 FROM {$wpdb->prefix}posts p
                 INNER JOIN {$wpdb->prefix}postmeta pm ON pm.post_id = p.ID
                 INNER JOIN {$wpdb->prefix}users u ON u.ID = p.post_author
-                WHERE p.post_type = 'shop_order'
-                AND pm.meta_key = '_cpdd_delivery_date'
+                WHERE p.post_type = %s
+                AND pm.meta_key = %s
                 ORDER BY delivery_date ASC";
-        $sql .= " LIMIT $per_page";
+        $sql .= " LIMIT %d";
         $sql .= ' OFFSET ' . ( $page_number - 1 ) * $per_page;
-        $result = $wpdb->get_results( $sql, 'ARRAY_A' );
+        $result = $wpdb->get_results( $wpdb->prepare( $sql, 'shop_order', '_cpdd_delivery_date', $per_page ), ARRAY_A );
+		
+		// Escape/Sanitize all the data before returning.
+		foreach ( $result as &$item ) {
+			foreach ( $item as $key => &$value ) {
+				$value = sanitize_text_field( $value );
+			}
+		}
+		
         return $result;
     }
 
@@ -104,16 +114,25 @@ class CP_Orders_With_Delivery_Dates_Table extends WP_List_Table {
         global $wpdb;
         $sql = "SELECT COUNT(*) FROM {$wpdb->prefix}posts p
                 INNER JOIN {$wpdb->prefix}postmeta pm ON pm.post_id = p.ID
-                WHERE p.post_type = 'shop_order'
-                AND pm.meta_key = '_cpdd_delivery_date'";
-        return $wpdb->get_var( $sql );
+                WHERE p.post_type = %s
+                AND pm.meta_key = %s";
+        return $wpdb->get_var( $wpdb->prepare( $sql, 'shop_order', '_cpdd_delivery_date' ) );
     }
 }
 
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'cpdd_plugin_action_links' );
+
+/**
+ * Add settings link to plugin list table
+ *
+ * @param  array $links Existing links
+ *
+ * @return array 		Modified links
+ */
 function cpdd_plugin_action_links( $links ) {
     $links[] = '<a href="' .
-        admin_url( 'admin.php?page=cpdd_settings' ) .
-        '">' . __('Settings', 'customize-product-delivery-date') . '</a>';
+        esc_url( admin_url( 'admin.php?page=cpdd_settings' ) ) .
+        '">' . esc_html__('Settings', 'customize-product-delivery-date') . '</a>';
     return $links;
 }
 
