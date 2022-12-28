cebf38fd85851274b8967ca9a2edd9baed52afa6
DtheRock <29987671+DtheRock@users.noreply.github.com>
2022-12-28 16:59:36 +0200

    Update dateorders-woo.php

--- dateorders-woo.php
+++ dateorders-woo.php
@@ -230,5 +230,12 @@ function wooproddel_display_message( $checkout ) {
     }
 }
 
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'wooproddel_plugin_action_links' );
+
+function wooproddel_plugin_action_links($links){
+    $links[] = '<a href="'. esc_url( get_admin_url(null, 'admin.php?page=wooproddel_settings') ) .'">Settings</a>';
+    return $links;
+}
+
 
 ?>
